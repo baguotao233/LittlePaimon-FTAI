291f46dcf64b84d18b454625dcac92eeba09fea0
启动台 <67402215+rdp-studio@users.noreply.github.com>
2022-09-11 10:08:29 +0800

    Create config.py

---
+++ LittlePaimon/plugins/Genshin_AIVoice/config.py
@@ -0,0 +1,16 @@
+from typing import Dict
+from LittlePaimon.config import AI_CONFIG
+from LittlePaimon.utils.files import load_yaml, save_yaml
+
+
+class AIConfig:
+    def __init__(self):
+        if AI_CONFIG.exists():
+            data = load_yaml(AI_CONFIG)
+        else:
+            data = {}
+        self.token: str = data.get('密钥', '请在 https://www.yuque.com/launchpad/kwmwcs/masia7 处申请')
+        self.save()
+
+
+config = AIConfig()
